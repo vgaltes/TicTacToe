30bb81f1d69d037f85d9b7a2b427a4c2e62070e2
vigarcia <vigarcia@plainconcepts.com>
2014-03-26 22:34:31 +0100

    Center strategy tests

---
+++ TicTacToeGame.Test/Strategies/CenterStrategyTest.cs
@@ -0,0 +1,58 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicTacToeGame.Strategies;
+
+namespace TicTacToeGame.Test.Strategies
+{
+    [TestClass]
+    public class CenterStrategyTest
+    {
+        [TestMethod]
+        public void GivenTheCenterIsFree_CanHandleReturnsTrue()
+        {
+            var initialBoard = GetEmptyBoard();
+
+            var centerStrategy = new CenterStrategy();
+            var canHandle = centerStrategy.CanHandle(initialBoard);
+
+            canHandle.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void GivenTheCenterIsNotFree_CanHandleReturnsFalse()
+        {
+            var initialBoard = GetABoardWithAMarkInTheCenterOfType(Cell.Opponent);
+
+            var centerStrategy = new CenterStrategy();
+            var canHandle = centerStrategy.CanHandle(initialBoard);
+
+            canHandle.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void GivenTheCenterIsFree_UpdatePutsAMarkInTheCenter()
+        {
+            var initialBoard = GetEmptyBoard();
+
+            var centerStrategy = new CenterStrategy();
+            centerStrategy.Update(initialBoard);
+
+            var expectedBoard = GetABoardWithAMarkInTheCenterOfType(Cell.AI);
+
+            initialBoard.Should().BeEquivalentTo(expectedBoard);
+        }
+
+        private static Cell[,] GetEmptyBoard()
+        {
+            return GetABoardWithAMarkInTheCenterOfType(Cell.Empty);
+        }
+
+        private static Cell[,] GetABoardWithAMarkInTheCenterOfType(Cell cell)
+        {
+            var board = new Cell[3, 3]{{Cell.Empty, Cell.Empty, Cell.Empty},
+                                                {Cell.Empty, cell, Cell.Empty},
+                                                {Cell.Empty, Cell.Empty, Cell.Empty}};
+            return board;
+        }
+    }
+}
--- TicTacToeGame.Test/TicTacToeTests.cs
+++ TicTacToeGame.Test/TicTacToeTests.cs
@@ -8,18 +8,7 @@ namespace TicTacToeGame.Test
     [TestClass]
     public class TicTacToeTests
     {
-        [TestMethod]
-        public void GivenANewGame_TheAIPutTheMarkInTheCenter()
-        {
-            var ticTacToe = new TicTacToe();
-            ticTacToe.AIMove();
-
-            var expectedBoard = new Cell[3,3]{{Cell.Empty, Cell.Empty, Cell.Empty},
-                                                {Cell.Empty, Cell.AI, Cell.Empty},
-                                                {Cell.Empty, Cell.Empty, Cell.Empty}};
-
-            ticTacToe.Board.Should().BeEquivalentTo(expectedBoard);
-        }
+        
 
         [TestMethod]
         public void GivenThereIsAnOpponentMarkInTheCenter_TheAIPutTheMarkInTheFirstCorner()
