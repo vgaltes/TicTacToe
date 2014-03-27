9a8b45c12fa729c3de55296e9ad53c535ea1c1cc
vigarcia <vigarcia@plainconcepts.com>
2014-03-27 23:29:33 +0100

    GetBoardRepresentationOfBoardWithAIInSomeCells_ReturnsTheBoardDrawnAsAString -> green

--- TicTacToeGame.Console.Test/TicTacToeBoardDrawer.cs
+++ TicTacToeGame.Console.Test/TicTacToeBoardDrawer.cs
@@ -9,7 +9,20 @@ namespace TicTacToeGame.Console.Test
     {
         internal string GetRepresentationOf(Cell[,] board)
         {
-            return " | | \n-----\n | | \n-----\n | | ";
+            return string.Format("{0}|{1}|{2}\n-----\n{3}|{4}|{5}\n-----\n{6}|{7}|{8}",
+                Draw(board[0, 0]), Draw(board[0, 1]), Draw(board[0, 2]),
+                Draw(board[1, 0]), Draw(board[1, 1]), Draw(board[1, 2]),
+                Draw(board[2, 0]), Draw(board[2, 1]), Draw(board[2, 2]));
+        }
+
+        private string Draw(Cell cell)
+        {
+            if (cell == Cell.AI)
+            {
+                return "X";
+            }
+
+            return " ";
         }
     }
 }
