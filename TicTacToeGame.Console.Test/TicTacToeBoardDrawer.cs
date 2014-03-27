6b9c31c7ab228d8c6c06d2067750e5ed66f5c4fc
vigarcia <vigarcia@plainconcepts.com>
2014-03-27 23:17:39 +0100

    GetBoardRepresentationOfAnEmptyBoard_ReturnsEmptyBoardDrawnAsAString -> red

---
+++ TicTacToeGame.Console.Test/TicTacToeBoardDrawer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TicTacToeGame.Console.Test
+{
+    class TicTacToeBoardDrawer
+    {
+        internal object GetRepresentationOf(Cell[,] board)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TicTacToeGame.Console.Test/TicTacToeBoardDrawerTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace TicTacToeGame.Console.Test
+{
+    [TestClass]
+    public class TicTacToeBoardDrawerTests
+    {
+        [TestMethod]
+        public void GetBoardRepresentationOfAnEmptyBoard_ReturnsEmptyBoardDrawnAsAString()
+        {
+            Cell[,] board = new Cell[3, 3]{{Cell.Empty, Cell.Empty, Cell.Empty},
+                                                {Cell.Empty, Cell.Empty, Cell.Empty},
+                                                {Cell.Empty, Cell.Empty, Cell.Empty}};
+
+            var ticTacToeBoardDrawer = new TicTacToeBoardDrawer();
+            var boardRepresentation = ticTacToeBoardDrawer.GetRepresentationOf(board);
+
+            var expectedRepresentation = " | | \n-----\n | | \n-----\n | | ";
+
+            boardRepresentation.Should().Be(expectedRepresentation);
+        }
+    }
+}
