f71a0ddfae6b6be5f7e41bd27068093d752b9654
vigarcia <vigarcia@plainconcepts.com>
2014-03-29 13:43:45 +0100

    GivenTheThirdSideFree_CanHandleReturnsTrue -> green

--- TicTacToeGame/Strategies/FreeSideStrategy.cs
+++ TicTacToeGame/Strategies/FreeSideStrategy.cs
@@ -10,6 +10,7 @@ namespace TicTacToeGame.Strategies
         {
             Sides.Add(new MarkCoordinate(0, 1));
             Sides.Add(new MarkCoordinate(1, 0));
+            Sides.Add(new MarkCoordinate(1, 2));
         }
 
         public bool CanHandle(Cell[,] board)
