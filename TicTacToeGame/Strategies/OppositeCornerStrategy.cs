82c9fd22b3a089c28fb81ec230874365541f13af
vigarcia <vigarcia@plainconcepts.com>
2014-03-29 12:15:39 +0100

    GivenThereIsAnOpponentInTheFirstAndFourthCorner_CanHandleReturnsFalse -> green

--- TicTacToeGame/Strategies/OppositeCornerStrategy.cs
+++ TicTacToeGame/Strategies/OppositeCornerStrategy.cs
@@ -1,15 +1,56 @@
-﻿namespace TicTacToeGame.Strategies
+﻿using System.Collections.Generic;
+namespace TicTacToeGame.Strategies
 {
+    public class MarkCoordinate
+    {
+        public int Row { get; private set; }
+        public int Column { get; private set; }
+
+        public MarkCoordinate(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+    }
+
     public class OppositeCornerStrategy : TicTacToeStrategy
     {
-        public bool CanHandle(Cell[,] board)
+        private Dictionary<MarkCoordinate, MarkCoordinate> CornersAndOpposites
+            = new Dictionary<MarkCoordinate, MarkCoordinate>();
+
+        public OppositeCornerStrategy()
         {
-            return ThereIsAnOpponentInASquare(board);
+            CornersAndOpposites.Add(new MarkCoordinate(0, 0), new MarkCoordinate(2, 2));
+            CornersAndOpposites.Add(new MarkCoordinate(0, 2), new MarkCoordinate(2, 0));
+            CornersAndOpposites.Add(new MarkCoordinate(2, 0), new MarkCoordinate(0, 2));
+            CornersAndOpposites.Add(new MarkCoordinate(2, 2), new MarkCoordinate(0, 0));
         }
 
+        public bool CanHandle(Cell[,] board)
+        {   
+            foreach ( var cornerAndOpposite in CornersAndOpposites )
+            {
+                if ( IsThereAndOpponentInTheCorner(cornerAndOpposite.Key, board) 
+                    && IsOppositeCornerFree(cornerAndOpposite.Value, board))
+                    return true;
+            }
+
+            return false;
+        }
+        
         public void Update(Cell[,] board)
         {
             PutAMarkInTheOppositeSquare(board);
+        }
+        
+        private bool IsOppositeCornerFree(MarkCoordinate markCoordinate, Cell[,] board)
+        {
+            return board[markCoordinate.Row, markCoordinate.Column] == Cell.Empty;
+        }
+
+        private bool IsThereAndOpponentInTheCorner(MarkCoordinate markCoordinate, Cell[,] board)
+        {
+            return board[markCoordinate.Row, markCoordinate.Column] == Cell.Opponent;
         }
 
         private bool ThereIsAnOpponentInASquare(Cell[,] board)
