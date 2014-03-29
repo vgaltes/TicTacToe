54bb67a65f43a2d894dc484bc7b55950374e6884
vigarcia <vigarcia@plainconcepts.com>
2014-03-29 13:04:16 +0100

    GivenAFullBoard_CanHandleReturnsFalse -> green

---
+++ TicTacToeGame.Test/Strategies/FreeSideStrategyTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+using TicTacToeGame.Strategies;
+
+namespace TicTacToeGame.Test.Strategies
+{
+    [TestClass]
+    public class FreeSideStrategyTest
+    {
+        [TestMethod]
+        public void GivenAFullBoard_CanHandleReturnsFalse()
+        {
+            var freeSideStrategy = new FreeSideStrategy();
+
+            var initialBoard = BoardTestHelper.GetFullBoard();
+
+            var canHandle = freeSideStrategy.CanHandle(initialBoard);
+
+            canHandle.Should().BeFalse();
+        }
+    }
+}
---
+++ TicTacToeGame/Strategies/FreeSideStrategy.cs
@@ -0,0 +1,10 @@
+﻿namespace TicTacToeGame.Strategies
+{
+    public class FreeSideStrategy
+    {
+        public bool CanHandle(Cell[,] initialBoard)
+        {
+            return false;
+        }
+    }
+}
