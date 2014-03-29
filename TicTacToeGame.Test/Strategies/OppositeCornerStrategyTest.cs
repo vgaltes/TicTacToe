5dee30f95507fcf2bae58bcd7766365d9d0c8cdd
vigarcia <vigarcia@plainconcepts.com>
2014-03-29 10:43:52 +0100

    Added missing files.... :(

---
+++ TicTacToeGame.Test/Strategies/OppositeCornerStrategyTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicTacToeGame.Strategies;
+
+namespace TicTacToeGame.Test.Strategies
+{
+    [TestClass]
+    public class OppositeCornerStrategyTest
+    {
+        [TestMethod]
+        public void GivenThereIsAnOpponentInTheFirstCorner_TheAIPutsTheMarkInTheThirdCorner()
+        {
+            var oppositeCornerStrategy = new OppositeCornerStrategy();
+
+            var initialBoard = BoardTestHelper.GetABoardWithAMark(Mark.OpponentFromCoordinates(0, 0));
+            oppositeCornerStrategy.Update(initialBoard);
+
+            var expectedBoard = BoardTestHelper.GetABoardWithMarks(new List<Mark>
+                {
+                    Mark.OpponentFromCoordinates(0, 0),
+                    Mark.AIFromCoordinates(2, 2)
+                });
+
+            initialBoard.Should().ContainInOrder(expectedBoard);
+        }
+
+        [TestMethod]
+        public void GivenThereIsAnOpponentInTheSecondCorner_TheAIPutsTheMarkInTheFourthCorner()
+        {
+            var oppositeCornerStrategy = new OppositeCornerStrategy();
+
+            var initialBoard = BoardTestHelper.GetABoardWithAMark(Mark.OpponentFromCoordinates(0, 2));
+
+            oppositeCornerStrategy.Update(initialBoard);
+
+            var expectedBoard = BoardTestHelper.GetABoardWithMarks(new List<Mark>
+                {
+                    Mark.OpponentFromCoordinates(0, 2),
+                    Mark.AIFromCoordinates(2, 0)
+                });
+
+            initialBoard.Should().ContainInOrder(expectedBoard);
+        }
+    }
+}
