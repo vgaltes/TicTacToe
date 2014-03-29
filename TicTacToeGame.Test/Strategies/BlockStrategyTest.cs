a6c0ed439f1ce51756261da37662dcc5f1a58876
vigarcia <vigarcia@plainconcepts.com>
2014-03-29 14:13:05 +0100

    GivenThereIsAMarkInAllCorners_CanHandleReturnsFalse -> red

---
+++ TicTacToeGame.Test/Strategies/BlockStrategyTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicTacToeGame.Strategies;
+
+namespace TicTacToeGame.Test.Strategies
+{
+    [TestClass]
+    public class BlockStrategyTest
+    {
+        [TestMethod]
+        public void GivenThereIsAMarkInAllCorners_CanHandleReturnsFalse()
+        {
+            var initialBoard = BoardTestHelper.GetABoardWithMarks(new List<Mark> {
+                Mark.OpponentFromCoordinates(0, 0),
+                Mark.OpponentFromCoordinates(0, 2),
+                Mark.OpponentFromCoordinates(2, 0),
+                Mark.OpponentFromCoordinates(2, 2)
+            });
+
+            var blockStrategy = new BlockStrategy();
+            var canHandle = blockStrategy.CanHandle(initialBoard);
+
+            canHandle.Should().BeFalse();
+        }
+    }
+}
---
+++ TicTacToeGame/Strategies/BlockStrategy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TicTacToeGame.Strategies
+{
+    public class BlockStrategy : TicTacToeStrategy
+    {
+        public bool CanHandle(Cell[,] board)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Cell[,] board)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
