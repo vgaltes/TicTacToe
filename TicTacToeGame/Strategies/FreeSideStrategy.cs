6697d99c48e043fe2c7c460037e88075676f9fa0
vigarcia <vigarcia@plainconcepts.com>
2014-03-29 13:37:42 +0100

    GivenTheFirstSideFree_CanHandleReturnsTrue -> green

--- TicTacToeGame/Strategies/FreeSideStrategy.cs
+++ TicTacToeGame/Strategies/FreeSideStrategy.cs
@@ -1,10 +1,19 @@
-﻿namespace TicTacToeGame.Strategies
+﻿using System.Collections.Generic;
+namespace TicTacToeGame.Strategies
 {
     public class FreeSideStrategy
     {
-        public bool CanHandle(Cell[,] initialBoard)
+        private List<MarkCoordinate> Sides
+            = new List<MarkCoordinate>();
+
+        public FreeSideStrategy()
         {
-            return false;
+            Sides.Add(new MarkCoordinate(0, 1));
+        }
+
+        public bool CanHandle(Cell[,] board)
+        {
+            return board[Sides[0].Row, Sides[0].Column] == Cell.Empty;
         }
     }
 }
