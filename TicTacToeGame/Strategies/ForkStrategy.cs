b287525d2c5364d94b5cd73b80bd519a544f4e6f
vigarcia <vigarcia@plainconcepts.com>
2014-03-30 19:10:40 +0200

    Refactor

--- TicTacToeGame/Strategies/ForkStrategy.cs
+++ TicTacToeGame/Strategies/ForkStrategy.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 namespace TicTacToeGame.Strategies
 {
     public class ForkStrategy : LineStrategy
@@ -9,27 +11,43 @@ namespace TicTacToeGame.Strategies
         {
             int suitableLines = 0;
 
-            for ( int row = 0; row < 3; row++ )
+            foreach (var emptyCell in EmptyCellsInBoard(board))
             {
-                for ( int column = 0; column < 3; column++)
+                foreach (var line in LinesWhichCrossesTheCell(emptyCell))
                 {
-                    if ( board[row, column] == Cell.Empty)
-                    {
-                        foreach ( var line in Lines)
-                        {
-                            if (line.LineStart.Row == row && line.LineStart.Column == column ||
-                                line.LineEnd.Row == row && line.LineEnd.Column == column ||
-                                line.Evaluate.Row == row && line.Evaluate.Column == column)
-                            {
-                                if ( IsLineSuitableForAFork(board, line))
-                                    suitableLines++;
-                            }   
-                        }
-                    }
+                    if (IsLineSuitableForAFork(board, line))
+                        suitableLines++;
                 }
             }
 
             return suitableLines >= 2;
+        }
+
+        private IEnumerable<MarkCoordinate> EmptyCellsInBoard(Cell[,] board)
+        {
+            for (int row = 0; row < 3; row++)
+            {
+                for (int column = 0; column < 3; column++)
+                {
+                    if (board[row, column] == Cell.Empty)
+                    {
+                        yield return new MarkCoordinate(row, column);
+                    }
+                }
+            }
+        }
+
+        private IEnumerable<Line> LinesWhichCrossesTheCell(MarkCoordinate cell)
+        {
+            foreach (var line in Lines)
+            {
+                if (line.LineStart.Row == cell.Row && line.LineStart.Column == cell.Column ||
+                    line.LineEnd.Row == cell.Row && line.LineEnd.Column == cell.Column ||
+                    line.Evaluate.Row == cell.Row && line.Evaluate.Column == cell.Column)
+                {
+                    yield return line;
+                }
+            }
         }
 
         private bool IsLineSuitableForAFork(Cell[,] board, Line line)
@@ -40,7 +58,7 @@ namespace TicTacToeGame.Strategies
                 board[line.LineEnd.Row, line.LineEnd.Column] == Cell.Opponent ||
                 board[line.Evaluate.Row, line.Evaluate.Column] == Cell.Opponent)
                 return false;
-            
+
             if (board[line.LineStart.Row, line.LineStart.Column] == Cell.AI)
                 aiCells++;
             if (board[line.LineEnd.Row, line.LineEnd.Column] == Cell.AI)
