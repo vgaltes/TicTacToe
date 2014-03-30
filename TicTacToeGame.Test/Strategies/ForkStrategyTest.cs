e6dc0725b5f03b59dc7258ed94d6f3e046aeee15
vigarcia <vigarcia@plainconcepts.com>
2014-03-30 17:33:04 +0200

    GivenAnEmptyBoard_CanHandleReturnsFalse -> red

---
+++ TicTacToeGame.Test/Strategies/ForkStrategyTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using FluentAssertions;
+using TicTacToeGame.Strategies;
+
+namespace TicTacToeGame.Test.Strategies
+{
+    [TestFixture]
+    public class ForkStrategyTest
+    {
+        [Test]
+        public void GivenAnEmptyBoard_CanHandleReturnsFalse()
+        {
+            var forkStrategy = new ForkStrategy();
+
+            var initialBoard = BoardTestHelper.GetAnEmptyBoard();
+
+            var canHandle = forkStrategy.CanHandle(initialBoard);
+
+            canHandle.Should().BeFalse();
+        }
+    }
+}
---
+++ TicTacToeGame/Strategies/ForkStrategy.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace TicTacToeGame.Strategies
+{
+    public class ForkStrategy
+    {
+        public bool CanHandle(Cell[,] board)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
