0ea88d09e18613472ed85c3a8f04afd58dd95602
vigarcia <vigarcia@plainconcepts.com>
2014-03-30 17:42:05 +0200

    GivenABoarkWithOneAIMark_CanHandleReturnsFalse -> green

--- TicTacToeGame.Test/Strategies/ForkStrategyTest.cs
+++ TicTacToeGame.Test/Strategies/ForkStrategyTest.cs
@@ -7,12 +7,22 @@ namespace TicTacToeGame.Test.Strategies
     [TestFixture]
     public class ForkStrategyTest
     {
+        ForkStrategy forkStrategy = new ForkStrategy();
+
         [Test]
         public void GivenAnEmptyBoard_CanHandleReturnsFalse()
         {
-            var forkStrategy = new ForkStrategy();
-
             var initialBoard = BoardTestHelper.GetAnEmptyBoard();
+
+            var canHandle = forkStrategy.CanHandle(initialBoard);
+
+            canHandle.Should().BeFalse();
+        }
+
+        [Test]
+        public void GivenABoarkWithOneAIMark_CanHandleReturnsFalse()
+        {
+            var initialBoard = BoardTestHelper.GetABoardWithAMark(Mark.AIFromCoordinates(0, 0));
 
             var canHandle = forkStrategy.CanHandle(initialBoard);
 
