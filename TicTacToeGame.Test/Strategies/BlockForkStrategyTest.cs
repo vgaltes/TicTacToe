deae9f36dd2b25299a779e240795c9f7e844eaf6
vigarcia <vigarcia@plainconcepts.com>
2014-04-08 22:06:38 +0200

    GivenThereAreCellsInTheFirstQuarter_CanHandleReturnsFalse -> false

--- TicTacToeGame.Test/Strategies/BlockForkStrategyTest.cs
+++ TicTacToeGame.Test/Strategies/BlockForkStrategyTest.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using TicTacToeGame.Strategies;
 using FluentAssertions;
+using System.Collections.Generic;
 
 namespace TicTacToeGame.Test.Strategies
 {
@@ -16,6 +17,23 @@ namespace TicTacToeGame.Test.Strategies
             var canHandle = blockForkStrategy.CanHandle(initialBoard);
 
             canHandle.Should().BeTrue();
+        }
+
+        [Test]
+        public void GivenThereAreCellsInTheFirstQuarter_CanHandleReturnsFalse()
+        {
+            var blockForkStrategy = new BlockForkStrategy();
+
+            var initialBoard = BoardTestHelper.GetABoardWithMarks(new List<Mark>{
+                Mark.AIFromCoordinates(0, 0),
+                Mark.OpponentFromCoordinates(0, 1),
+                Mark.OpponentFromCoordinates(1, 0),
+                Mark.OpponentFromCoordinates(1, 1),
+            });
+
+            var canHandle = blockForkStrategy.CanHandle(initialBoard);
+
+            canHandle.Should().BeFalse();
         }
     }
 }
