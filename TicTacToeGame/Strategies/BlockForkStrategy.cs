a5e190264ef51c31af35784b1b7c899ef4aecbe0
vigarcia <vigarcia@plainconcepts.com>
2014-04-08 22:15:23 +0200

    GivenThereAreCellsInTheFirstQuarter_CanHandleReturnsFalse -> green

--- TicTacToeGame/Strategies/BlockForkStrategy.cs
+++ TicTacToeGame/Strategies/BlockForkStrategy.cs
@@ -1,17 +1,63 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TicTacToeGame.Strategies
 {
-    public class BlockForkStrategy : TicTacToeStrategy
+    public class BlockForkStrategy : LineStrategy
     {
+        public BlockForkStrategy() : base(Cell.AI) { }
+
         public bool CanHandle(Cell[,] board)
         {
-            return true;
+            foreach (var emptyCell in EmptyCellsInBoard(board))
+            {
+                var imaginaryBoard = new Cell[3, 3];
+                Array.Copy(board, imaginaryBoard, board.Length);
+                imaginaryBoard[emptyCell.Row, emptyCell.Column] = Cell.AI;
+
+                foreach (var line in Lines)
+                {
+                    if (IsLineSuitableForABlockFork(imaginaryBoard, line))
+                        return true;
+                }
+            }
+
+            return false;
         }
 
         public void Update(Cell[,] board)
         {
             throw new NotImplementedException();
+        }
+
+        private IEnumerable<MarkCoordinate> EmptyCellsInBoard(Cell[,] board)
+        {
+            for (int row = 0; row < 3; row++)
+            {
+                for (int column = 0; column < 3; column++)
+                {
+                    if (board[row, column] == Cell.Empty)
+                    {
+                        yield return new MarkCoordinate(row, column);
+                    }
+                }
+            }
+        }
+
+        private bool IsLineSuitableForABlockFork(Cell[,] board, Line line)
+        {
+            int aiCells = 0;
+
+            foreach (var coordinate in line.Coordinates)
+            {
+                if (board[coordinate.Row, coordinate.Column] == Cell.Opponent)
+                    return false;
+
+                if (board[coordinate.Row, coordinate.Column] == Cell.AI)
+                    aiCells++;
+            }
+
+            return aiCells == 2;
         }
     }
 }
