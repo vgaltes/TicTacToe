3e13cfa2fb60ec32b4910a13dcc85e482b084816
vigarcia <vigarcia@plainconcepts.com>
2014-04-12 18:58:10 +0200

    Eliminate duplication in ForkStrategy

--- TicTacToeGame/Strategies/ForkStrategy.cs
+++ TicTacToeGame/Strategies/ForkStrategy.cs
@@ -8,6 +8,20 @@ namespace TicTacToeGame.Strategies
     {
         public bool CanHandle(Board board)
         {
+            return GetCellCoordinatesSuitableForFork(board) != null;
+        }
+
+        public void Update(Board board)
+        {
+            var cellCoordinatesSuitableForFork = GetCellCoordinatesSuitableForFork(board);
+            if ( cellCoordinatesSuitableForFork != null)
+            {
+                board.FillAICell(cellCoordinatesSuitableForFork);
+            }
+        }
+
+        private MarkCoordinate GetCellCoordinatesSuitableForFork(Board board)
+        {
             foreach (var emptyCell in board.EmptyCells)
             {
                 int suitableLines = 0;
@@ -21,33 +35,10 @@ namespace TicTacToeGame.Strategies
                 }
 
                 if (suitableLines >= 2)
-                    return true;
+                    return emptyCell;
             }
 
-            return false;
-        }
-
-        public void Update(Board board)
-        {
-            
-            foreach (var emptyCell in board.EmptyCells)
-            {
-                int suitableLines = 0;
-
-                var imaginaryBoard = board.GetCopyWithExtraCellOfType(Cell.AI, emptyCell);
-
-                foreach ( var line in board.Lines)
-                { 
-                    if (IsLineSuitableForAFork(imaginaryBoard, line))
-                            suitableLines++;
-                }
-
-                if (suitableLines >= 2)
-                {
-                    board.FillAICell(emptyCell);
-                    return;
-                }
-            }            
+            return null;
         }
 
         private bool IsLineSuitableForAFork(Board board, Line line)
