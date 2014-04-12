93621f1786a07b5720e8ac9df19b6dfe015d8eab
vigarcia <vigarcia@plainconcepts.com>
2014-04-12 21:26:58 +0200

    WhenThePlayerPlays_TheGamePlaysTheAI -> red

---
+++ TicTacToeGame.Test/TicTacToeTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using TicTacToeGame.Models;
+using TicTacToeGame.Strategies;
+
+namespace TicTacToeGame.Test
+{
+    [TestFixture]
+    public class TicTacToeTest
+    {
+        [Test]
+        public void WhenThePlayerPlays_TheGamePlaysTheAI()
+        {
+            var board = new Mock<Board>();
+            var ticTacToe = new TicTacToe(board.Object, new List<TicTacToeStrategy>());
+
+            ticTacToe.OpponentMove(new Models.CellCoordinates(0, 0));
+
+            board.Verify(b => b.FillAICell(It.IsAny<CellCoordinates>()));
+        }
+    }
+}
