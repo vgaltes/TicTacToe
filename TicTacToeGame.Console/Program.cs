73da7d550ff999fe4742f4d8fc01b7c7f541bf44
vigarcia <vigarcia@plainconcepts.com>
2014-04-13 10:51:29 +0200

    Refactor program.

--- TicTacToeGame.Console/Program.cs
+++ TicTacToeGame.Console/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using TicTacToeGame.Exceptions;
 using TicTacToeGame.Models;
 using TicTacToeGame.Strategies;
 
@@ -10,22 +9,63 @@ namespace TicTacToeGame.Console
 {
     class Program
     {
+        private const string MOVEMENT_NOT_ALLOWED =
+            "MOVEMENT NOT ALLOWED. PLEASE ENTER NEW COORDINATES.";
+        private const string UNEXPECTED_EXCEPTION =
+            "UNEXPECTED EXCEPTION HAS OCURRED. PLEASE ENTER NEW COORDINATES.";
+        private const string QUIT_COMMAND = "q!";
+
+        private static string extraInfo = string.Empty;
+
         static void Main(string[] args)
         {
             var ticTacToe = CreateTicTacToe();
             var ticTacToeBoardDrawer = new TicTacToeBoardDrawer();
-            var boardAsString = ticTacToeBoardDrawer.GetRepresentationOf(ticTacToe.Board);
-            System.Console.WriteLine(boardAsString);
-            string line = System.Console.ReadLine();
+
+            DrawBoard(ticTacToe, ticTacToeBoardDrawer);
+
+            string userInput = GetUserInput();
             do
             {
-                int[] coordinates = line.Split(',').Select(c => int.Parse(c)).ToArray();
-                ticTacToe.OpponentMove(new CellCoordinates(coordinates[0], coordinates[1]));
+                try
+                {
+                    extraInfo = string.Empty;
+                    ticTacToe.OpponentMove(GetCoordinatesFromUserInput(userInput));
+                }
+                catch ( NotAllowedMovementException)
+                {
+                    extraInfo = MOVEMENT_NOT_ALLOWED;
+                }
+                catch (Exception)
+                {
+                    extraInfo = UNEXPECTED_EXCEPTION;
+                }
 
-                boardAsString = ticTacToeBoardDrawer.GetRepresentationOf(ticTacToe.Board);
-                System.Console.WriteLine(boardAsString);
-                line = System.Console.ReadLine();
-            } while (line != "q!");
+                DrawBoard(ticTacToe, ticTacToeBoardDrawer);
+                userInput = GetUserInput();
+
+            } while (userInput != QUIT_COMMAND);
+        }
+
+        private static string GetUserInput()
+        {
+            string line = System.Console.ReadLine();
+            return line;
+        }
+
+        private static CellCoordinates GetCoordinatesFromUserInput(string userInput)
+        {
+            int[] coordinates = userInput.Split(',').Select(c => int.Parse(c)).ToArray();
+            return new CellCoordinates(coordinates[0], coordinates[1]);
+        }
+
+        private static void DrawBoard(TicTacToe ticTacToe, TicTacToeBoardDrawer ticTacToeBoardDrawer)
+        {
+            System.Console.Clear();
+
+            string boardAsString = ticTacToeBoardDrawer.GetRepresentationOf(ticTacToe.Board);
+            System.Console.WriteLine(extraInfo);
+            System.Console.WriteLine(boardAsString);
         }
 
         private static TicTacToe CreateTicTacToe()
