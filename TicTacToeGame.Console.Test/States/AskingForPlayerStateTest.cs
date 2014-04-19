ffe53e067faca79f1cd964ebd6862c82da14a33a
vigarcia <vigarcia@plainconcepts.com>
2014-04-19 23:40:09 +0200

    IfUserChoosesANonIntegerOption_ExtraInfoIsSettedWithTheErrorInformation -> green

--- TicTacToeGame.Console.Test/States/AskingForPlayerStateTest.cs
+++ TicTacToeGame.Console.Test/States/AskingForPlayerStateTest.cs
@@ -12,6 +12,7 @@ namespace TicTacToeGame.Console.Test.States
         private const string AI_PLAYER = "1";
         private const string HUMAN_PLAYER = "2";
         private const string INVALID_OPTION = "3";
+        private const string NON_INTEGER_OPTION = "a";
 
         Mock<ITicTacToe>  ticTacToe;
         Mock<TicTacToeBoardDrawer> ticTacToeBoardDrawer;
@@ -81,6 +82,15 @@ namespace TicTacToeGame.Console.Test.States
         public void IfUserChoosesNorOpponentNeitherAI_ExtraInfoIsSettedWithTheErrorInformation()
         {
             consoleIO.Setup(c => c.ReadLine()).Returns(INVALID_OPTION);
+            asking4PlayerState.Evaluate();
+
+            tttConsoleRunner.State.InfoFromPreviousStep.Should().Be(Resources.SelectPlayer_InvalidOption);
+        }
+
+        [Test]
+        public void IfUserChoosesANonIntegerOption_ExtraInfoIsSettedWithTheErrorInformation()
+        {
+            consoleIO.Setup(c => c.ReadLine()).Returns(NON_INTEGER_OPTION);
             asking4PlayerState.Evaluate();
 
             tttConsoleRunner.State.InfoFromPreviousStep.Should().Be(Resources.SelectPlayer_InvalidOption);
