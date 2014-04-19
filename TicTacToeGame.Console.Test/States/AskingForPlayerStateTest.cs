ab4fe4a093c890c421658ff62e03c0069249f514
vigarcia <vigarcia@plainconcepts.com>
2014-04-19 16:55:24 +0200

    IfUserChoosesAI_TicTacToeSetInitialPlayerIsCalledWitCellTypeAI -> red

--- TicTacToeGame.Console.Test/States/AskingForPlayerStateTest.cs
+++ TicTacToeGame.Console.Test/States/AskingForPlayerStateTest.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework;
 using FluentAssertions;
 using TicTacToeGame.Console.States;
+using TicTacToeGame.Models;
 
 namespace TicTacToeGame.Console.Test.States
 {
@@ -25,6 +26,23 @@ namespace TicTacToeGame.Console.Test.States
             asking4PlayerState.Run();
 
             tttConsoleRunner.State.Should().BeOfType(typeof(PlayingState));
+        }
+
+        [Test]
+        public void IfUserChoosesAI_TicTacToeSetInitialPlayerIsCalledWitCellTypeAI()
+        {
+            var ticTacToe = new Mock<ITicTacToe>();
+            var ticTacToeBoardDrawer = new Mock<TicTacToeBoardDrawer>();
+            var consoleIO = new Mock<ConsoleIO>();
+            var tttConsoleRunner = new TicTacToeConsoleRunner(ticTacToe.Object, ticTacToeBoardDrawer.Object, consoleIO.Object);
+
+            var asking4PlayerState = new AskingForPlayerState(tttConsoleRunner);
+
+            consoleIO.Setup(c => c.ReadLine()).Returns(AI_PLAYER);
+
+            asking4PlayerState.Run();
+
+            ticTacToe.Verify(ttt => ttt.SetInitialPlayer(CellType.AI), Times.Once());
         }
     }
 }
