58dca95b88956bd0cc70e66c1d5b3028593e0584
vigarcia <vigarcia@plainconcepts.com>
2014-04-20 23:18:52 +0200

    ResetTicTacToe -> green

--- TicTacToeGame.Console/States/DrawState.cs
+++ TicTacToeGame.Console/States/DrawState.cs
@@ -21,6 +21,7 @@ namespace TicTacToeGame.Console.States
         {
             this.TicTacToeConsoleRunner.consoleIO.WriteLine(Resources.Draw);
             this.TicTacToeConsoleRunner.consoleIO.ReadKey();
+            this.TicTacToeConsoleRunner.ticTacToe.Reset();
             this.TicTacToeConsoleRunner.State = new AskingForPlayerState(this);
         }
     }
