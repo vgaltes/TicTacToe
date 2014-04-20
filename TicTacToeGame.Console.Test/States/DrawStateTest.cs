1d709fee491d71622e452570e26a08ccb0d2255f
vigarcia <vigarcia@plainconcepts.com>
2014-04-20 21:28:14 +0200

    ShowsDrawMessage -> red

---
+++ TicTacToeGame.Console.Test/States/DrawStateTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using FluentAssertions;
+using Moq;
+using TicTacToeGame.Console.States;
+
+namespace TicTacToeGame.Console.Test.States
+{
+    [TestFixture]
+    public class DrawStateTest
+    {
+        Mock<ITicTacToe> ticTacToe;
+        Mock<TicTacToeBoardDrawer> ticTacToeBoardDrawer;
+        Mock<ConsoleIO> consoleIO;
+        TicTacToeConsoleRunner tttConsoleRunner;
+        DrawState drawState;
+
+        [SetUp]
+        public void TestSetUp()
+        {
+            ticTacToe = new Mock<ITicTacToe>();
+            ticTacToeBoardDrawer = new Mock<TicTacToeBoardDrawer>();
+            consoleIO = new Mock<ConsoleIO>();
+            tttConsoleRunner = new TicTacToeConsoleRunner(ticTacToe.Object, ticTacToeBoardDrawer.Object, consoleIO.Object);
+
+            drawState = new DrawState(tttConsoleRunner);
+            tttConsoleRunner.State = drawState;
+        }
+
+        [Test]
+        public void ShowsDrawMessage()
+        {
+            drawState.Evaluate();
+
+            consoleIO.Verify(c => c.WriteLine(Resources.Draw));
+        }
+    }
+}
