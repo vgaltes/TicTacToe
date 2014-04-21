df830e7f8bfdd85c3fc4478f1702c5edabd9e1fc
vigarcia <vigarcia@plainconcepts.com>
2014-04-21 12:07:59 +0200

    WhenMovingForTheFirstTime_CallPlayer1Play -> green

--- TicTacToeGame.Console/States/PlayingState.cs
+++ TicTacToeGame.Console/States/PlayingState.cs
@@ -16,6 +16,9 @@ namespace TicTacToeGame.Console.States
 
         private Dictionary<TicTacToeState, Type> nextStates;
 
+        private Player player1;
+        private Player player2;
+
         public PlayingState(TicTacToeConsoleRunnerState state)
         {
             this.TicTacToeConsoleRunner = state.TicTacToeConsoleRunner;
@@ -26,6 +29,8 @@ namespace TicTacToeGame.Console.States
         public PlayingState(TicTacToeConsoleRunner tttConsoleRunner, Player player1, Player player2)
         {
             this.TicTacToeConsoleRunner = tttConsoleRunner;
+            this.player1 = player1;
+            this.player2 = player2;
             SetNextStates();
         }
 
@@ -34,6 +39,9 @@ namespace TicTacToeGame.Console.States
             WriteInfoFromPreviousStep();
             this.TicTacToeConsoleRunner.consoleIO.WriteLine(Resources.WriteCoordinates);
             DrawBoard();
+
+            player1.Move();
+
             string userInput = this.TicTacToeConsoleRunner.consoleIO.ReadLine();
 
             if (userInput == QUIT_COMMAND)
